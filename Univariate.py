f5e0496c5298d542de8181bf280e524f1a654c91
VenkateshwaraJ <104878847+VenkateshwaraJ@users.noreply.github.com>
2022-05-08 20:32:03 +0530

    Add files via upload

---
+++ Univariate.py
@@ -0,0 +1,15 @@
+class univariate():
+    
+
+    def QuanQual(self,dataset):
+        Quan=[]
+        Qual=[]
+        for columnname in dataset.columns:
+            print(columnname)
+        if(dataset[columnname].dtypes=="O"):
+            #print("Qual")
+            Quan.append(columnname)
+        else:
+            #print("Quan")
+            Qual.append(columnname)
+        return Quan,Qual
